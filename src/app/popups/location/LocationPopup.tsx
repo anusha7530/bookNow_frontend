77720fa15175079ab0834eb9cbed746716300ed2
anusha7530 <agarwalanusha13@gmail.com>
2025-04-03 15:45:08 +0530

    Select city api

--- src/app/popups/location/LocationPopup.tsx
+++ src/app/popups/location/LocationPopup.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import React from "react";
+import { toast } from 'react-toastify'
 const LocationPopup = ({
   setShowLocationPopup,
 }: {
   setShowLocationPopup: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
   const [cities, setCities] = React.useState<any[]>([]);
+  const [selectedCity, setSelectedCity] = React.useState<any>(null)
 
   const getcities = async () => {
     const indianCities = [
@@ -48,13 +50,41 @@ const LocationPopup = ({
   }, []);
 
   const handleSave = () => {
-    setShowLocationPopup(false);
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/changeCity`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+          city: selectedCity
+      })
+  })
+      .then((res) => res.json())
+      .then((data) => {
+          if (data.ok) {
+              setShowLocationPopup(false)
+              window.location.reload()
+          }
+          else{
+            toast(data.message, {
+              type: 'error'
+          })
+          }
+      })
+      .catch((err) => {
+          toast(err.message, {
+              type: 'error'
+          })
+          console.log(err)
+      })
   };
 
   return (
     <div className="fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center backdrop-blur-md">
       <div className="w-[90%] sm:w-[60%] md:w-[40%] lg:w-[30%] h-[30%] bg-white rounded-lg flex flex-col justify-center items-center gap-5 p-5 text-black  border border-[#b92d14e8]">
-        <select className="w-[90%] rounded-full shadow-md border-none outline-none py-2 px-5 text-lg cursor-pointer">
+        <select className="w-[90%] rounded-full shadow-md border-none outline-none py-2 px-5 text-lg cursor-pointer" onChange={(e) => {
+                         setSelectedCity(e.target.value)}}>
           <option value="" disabled selected>
             Select your city
           </option>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
-"use client"
-import React from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import { BiUserCircle, BiSearch } from "react-icons/bi";
 import { RiArrowDropDownFill } from "react-icons/ri";
@@ -10,6 +11,90 @@ import LocationPopup from "@/app/popups/location/LocationPopup";
 const Navbar = () => {
   const [showLocationPopup, setShowLocationPopup] =
     React.useState<boolean>(false);
+  const [user, setUser] = React.useState<any>(null);
+  const [loggedIn, setLoggedIn] = React.useState<boolean>(false);
+
+  const getuser = async () => {
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/getuser`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((response) => {
+        if (response.ok) {
+          console.log(response);
+          setUser(response.data);
+          setLoggedIn(true);
+        } else {
+          setLoggedIn(false);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleLogout = async () => {
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/logout`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.ok) {
+          window.location.href = "/auth/signin";
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        window.location.href = "/auth/signin";
+      });
+  };
+
+  // const checkLogin = async () => {
+  //   // let authToken = await getCookie('authToken')
+  //   // let refreshToken = await getCookie('refreshToken')
+
+  //   // console.log(authToken, refreshToken)
+  //   fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/checklogin`, {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     credentials: "include",
+  //   })
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((response) => {
+  //       console.log(response);
+  //       if (response.ok) {
+  //         setLoggedIn(true);
+  //       } else {
+  //         setLoggedIn(false);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       setLoggedIn(false);
+  //     });
+  // };
+
+  useEffect(() => {
+    // checkLogin();
+    getuser();
+  }, []);
   return (
     <nav className="flex justify-between items-center px-6 md:px-10 py-2 bg-gray-800 text-white">
       <div className="flex items-center gap-4 w-full md:w-auto">
@@ -25,8 +110,12 @@ const Navbar = () => {
       </div>
 
       <div className="flex items-center gap-6">
-        <p className="flex items-center cursor-pointer" onClick={() => setShowLocationPopup(true)}>
-          Mumbai <RiArrowDropDownFill className="text-xl" />
+        <p
+          className="flex items-center cursor-pointer"
+          onClick={() => setShowLocationPopup(true)}
+        >
+          {user ? user.city : "Select City"}
+          <RiArrowDropDownFill className="text-xl" />
         </p>
 
         <div className="md:hidden">
@@ -34,12 +123,19 @@ const Navbar = () => {
         </div>
 
         <div className="flex items-center gap-3">
-          <Link
-            href="/"
+          {
+            loggedIn ? 
+            <button className="hidden md:inline-block bg-[#b92d14e8] px-4 py-2 rounded-lg" onClick={handleLogout}>
+              Logout
+            </button>
+            :
+            <Link
+            href="/auth/signin"
             className="hidden md:inline-block bg-[#b92d14e8] px-4 py-2 rounded-lg"
           >
-            Logout
+            Login
           </Link>
+          }
           <Link href="/" className="flex items-center gap-1">
             <BiUserCircle className="text-2xl" />
           </Link>
