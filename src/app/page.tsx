da1a99fca50a445e24ac728038ef257cb8686c06
anusha7530 <agarwalanusha13@gmail.com>
2025-04-06 12:31:08 +0530

    Home page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,51 @@
 "use client";
 import HomeSlider from "@/components/HomeSlider/HomeSlider";
 import MovieCarousel from "@/components/MovieCarousel/MovieCarousel";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isUser, setIsUser] = useState(false);
+
+  const checkLogin = async () => {
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/checklogin`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include'
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((response) => {
+        console.log(response)
+        if (response.ok) {
+          setIsUser(true);
+        } else {
+          setIsUser(false);
+          window.location.href = "/auth/signin"
+        }
+      })
+      .catch((error) => {
+        setIsUser(false);
+        window.location.href = "/auth/signin"
+      })
+  };
+
+  useEffect(() => {
+    checkLogin();
+  }, [])
   return (
-    <main className="">
+    <>
+    {isUser && 
+      <main className="">
       <HomeSlider />
       <div className="m-10">
         <p className="text-4xl font-bold m-4">Featured Movies</p>
         <MovieCarousel />
       </div>
     </main>
+    }
+    </>
   );
 }
